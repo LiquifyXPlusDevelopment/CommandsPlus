ba9c1026dde1f8150301fed786cdc7a434c2732f
ofirtim <ofirrr32@gmail.com>
2022-03-30 10:55:11 +0300

    Fixed history command

--- src/main/java/dev/gdalia/commandsplus/commands/HistoryCommand.java
+++ src/main/java/dev/gdalia/commandsplus/commands/HistoryCommand.java
@@ -71,7 +71,7 @@ public class HistoryCommand implements CommandExecutor {
 		sender.sendMessage(CentredMessage.generate("&7&m&l               |&e " + target.getName() + " punish log &7&m&l|               &r"));
 		history.forEach(punishment -> {
 			sender.sendMessage(Message.fixColor("&ePunishment Id: &b" + punishment.getPunishmentUniqueId().toString()));
-			Optional.ofNullable(punishment.getExecuter()).ifPresent(uuid -> sender.sendMessage(Message.fixColor("&eExecuted by: &b" + Bukkit.getOfflinePlayer(uuid).getName())));
+			Optional.ofNullable(punishment.getExecuter()).ifPresent(uuid -> sender.sendMessage(Message.fixColor("&eExecuted by: &b" + Bukkit.getOfflinePlayer(punishment.getExecuter()).getName())));
 			sender.sendMessage(Message.fixColor("&eType: &b" + punishment.getType().getDisplayName()));
 
 			if (!List.of(PunishmentType.TEMPMUTE, PunishmentType.TEMPBAN, PunishmentType.KICK, PunishmentType.WARN).contains(punishment.getType())) {
