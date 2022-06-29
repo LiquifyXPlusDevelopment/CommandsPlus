1348e51ead3d3352a1ee162b2ce97b15b84d06b3
Gdalia23 <74269186+Gdalia23@users.noreply.github.com>
2022-06-29 23:40:42 +0300

    Invoke ReportManager.java

--- src/main/java/dev/gdalia/commandsplus/models/ReportManager.java
+++ src/main/java/dev/gdalia/commandsplus/models/ReportManager.java
@@ -1,4 +1,41 @@
 package dev.gdalia.commandsplus.models;
 
+import dev.gdalia.commandsplus.Main;
+import dev.gdalia.commandsplus.structs.reports.Report;
+import dev.gdalia.commandsplus.utils.Config;
+import lombok.Getter;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Optional;
+
 public class ReportManager {
+
+    @Getter
+    private static ReportManager instance = new ReportManager();
+
+    public void invoke(Report report) {
+        Config config = Main.getPunishmentsConfig();
+
+        Reports.getInstance().getReport(report.getConvicted()).ifPresent(activeReport ->
+                Reports.getInstance().writeTo(activeReport, ConfigFields.ReportsFields.REPORTED, true, false));
+
+        ConfigurationSection section = config.createSection(report.getReportUuid().toString());
+
+        section.set(ConfigFields.ReportsFields.REPORTED, report.getConvicted().toString());
+
+        Optional.of(report.getReporter()).ifPresent(uniqueId ->
+                section.set(ConfigFields.ReportsFields.REPORTER, report.getReporter().toString()));
+
+        section.set(ConfigFields.ReportsFields.REASON, report.getReason().getDisplayName());
+
+        Optional.of(report.getSentAt()).ifPresent(uniqueId ->
+                section.set(ConfigFields.ReportsFields.DATE, report.getSentAt().toEpochMilli()));
+
+        section.set(ConfigFields.ReportsFields.REASON, report.getReason());
+
+        config.saveConfig();
+    }
+
+    public void revoke() {
+    }
 }
