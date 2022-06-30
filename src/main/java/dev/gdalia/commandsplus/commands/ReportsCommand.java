ef31a32d6efab02635329a0397ea8b5c0c5b1c27
ofirtim <ofirrr32@gmail.com>
2022-06-30 16:01:41 +0300

    NOT READY LOL HAHA

--- src/main/java/dev/gdalia/commandsplus/commands/ReportsCommand.java
+++ src/main/java/dev/gdalia/commandsplus/commands/ReportsCommand.java
@@ -1,15 +1,21 @@
 package dev.gdalia.commandsplus.commands;
 
+import dev.gdalia.commandsplus.models.Reports;
 import dev.gdalia.commandsplus.structs.Message;
 import dev.gdalia.commandsplus.structs.Permission;
+import dev.gdalia.commandsplus.structs.reports.Report;
 import dev.gdalia.commandsplus.ui.ReportUI;
 import dev.gdalia.commandsplus.utils.CommandAutoRegistration;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 @CommandAutoRegistration.Command(value = {"reports"})
 public class ReportsCommand implements CommandExecutor {
@@ -32,7 +38,23 @@ public class ReportsCommand implements CommandExecutor {
             return false;
         }
 
-        new ReportUI(player).openReportsGUI(0);
+        if (args.length == 0) {
+            Message.playSound(sender, Sound.BLOCK_NOTE_BLOCK_HARP, 1, 1);
+            Message.REPORT_SUCCESSFULLY.sendMessage(sender, true);
+            return true;
+        }
+
+        OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+        if (!target.hasPlayedBefore()) {
+            Message.playSound(sender, Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+            Message.INVALID_PLAYER.sendMessage(sender, true);
+            return true;
+        }
+
+        /*
+        TODO Open report history UI for the executor.
+        new ReportUI(player).openReportGUI(target.getUniqueId());
+         */
         Message.playSound(player, Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1);
         return true;
     }
