a6001f983ed99d00d48891f9176918765b14bc77
ofirtim <ofirrr32@gmail.com>
2022-12-03 22:47:13 +0200

    Changed PunishmentRevoke.java extension to Punishment.java Now PunishmentRevoke is officially a punishment.

--- src/main/java/dev/gdalia/commandsplus/structs/punishments/PunishmentRevoke.java
+++ src/main/java/dev/gdalia/commandsplus/structs/punishments/PunishmentRevoke.java
@@ -1,22 +1,19 @@
 package dev.gdalia.commandsplus.structs.punishments;
 
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.UUID;
 
-import javax.annotation.Nullable;
+public class PunishmentRevoke extends Punishment {
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-
-@AllArgsConstructor
-public class PunishmentRevoke {
-
-	@Getter
-	@NonNull
-	private final Punishment punishment;
-	
 	@Getter
 	@Nullable
 	private final UUID removedBy;
-	
+
+	public PunishmentRevoke(@NotNull Punishment punishment, @Nullable UUID removedBy) {
+		super(punishment.getPunishmentUniqueId(), punishment.getPunished(), punishment.getExecutor(), punishment.getType(), punishment.getReason());
+		this.removedBy = removedBy;
+	}
 }
