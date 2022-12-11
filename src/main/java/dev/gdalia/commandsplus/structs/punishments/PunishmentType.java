eb8d0fd3362914c91660df17b9930d8ddd3db168
ofirtim <ofirrr32@gmail.com>
2022-12-11 21:50:18 +0200

    Punishment Types hold their message behaviours now.

--- src/main/java/dev/gdalia/commandsplus/structs/punishments/PunishmentType.java
+++ src/main/java/dev/gdalia/commandsplus/structs/punishments/PunishmentType.java
@@ -1,5 +1,6 @@
 package dev.gdalia.commandsplus.structs.punishments;
 
+import dev.gdalia.commandsplus.structs.Message;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,22 +8,102 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PunishmentType {
 	
-	BAN(true, true, "Ban", "Banned"),
-	TEMPBAN(false, true, "Tempban", "Tempbanned"),
-	MUTE(true, false, "Mute", "Muted"),
-	TEMPMUTE(false, false, "Tempmute", "Tempmuted"),
-	KICK(false, true, "Kick", "Kicked"),
-	WARN(false, false, "Warn", "Warned");
+	BAN(
+			true,
+			true,
+			true,
+			"Ban",
+			"Banned",
+			Message.PUNISH_BAN_SUBMITTED,
+			Message.PUNISH_BAN_ALREADY_BANNED,
+			null,
+			Message.PUNISH_REVOKE_UNBAN,
+			Message.PUNISH_BAN_NOT_BANNED,
+			Message.PUNISH_BAN_BROADCAST),
+
+	TEMPBAN(false,
+			true,
+			true,
+			"Tempban",
+			"Tempbanned",
+			Message.PUNISH_TEMPBAN_SUBMITTED,
+			Message.PUNISH_TEMPBAN_ALREADY_BANNED,
+			null,
+			Message.PUNISH_REVOKE_UNBAN,
+			Message.PUNISH_BAN_NOT_BANNED,
+			Message.PUNISH_TEMPBAN_BROADCAST),
+
+	MUTE(
+			true,
+			false,
+			true,
+			"Mute",
+			"Muted",
+			Message.PUNISH_MUTE_SUBMITTED,
+			Message.PUNISH_MUTE_ALREADY_MUTED,
+			Message.PUNISH_MUTE_MESSAGE,
+			Message.PUNISH_REVOKE_UNMUTE,
+			Message.PUNISH_MUTE_NOT_MUTED,
+			Message.PUNISH_MUTE_BROADCAST),
+
+	TEMPMUTE(
+			false,
+			false,
+			true,
+			"Tempmute",
+			"Tempmuted",
+			Message.PUNISH_TEMPMUTE_SUBMITTED,
+			Message.PUNISH_TEMPMUTE_ALREADY_MUTED,
+			Message.PUNISH_TEMPMUTE_MESSAGE,
+			Message.PUNISH_REVOKE_UNMUTE,
+			Message.PUNISH_MUTE_NOT_MUTED,
+			Message.PUNISH_TEMPMUTE_BROADCAST),
+
+	KICK(
+			false,
+			true,
+			false,
+			"Kick",
+			"Kicked",
+			Message.PUNISH_KICK_SUBMITTED,
+			null,
+			null,
+			null,
+			null,
+			Message.PUNISH_KICK_BROADCAST),
+
+	WARN(
+			false,
+			false,
+			false,
+			"Warn",
+			"Warned",
+			Message.PUNISH_WARN_SUBMITTED,
+			null,
+			Message.PUNISH_WARN_MESSAGE,
+			null,
+			null,
+			Message.PUNISH_WARN_BROADCAST);
 	
 	@Getter
 	private final boolean
 		permanent,
-		kickable;
+		kickable,
+		constrictive;
 	
 	@Getter
 	private final String 
 		displayName,
 		nameAsPunishMsg;
+
+	@Getter
+	private final Message
+		punishSuccessfulMessage,
+		alreadyPunishedMessage,
+		punishTargetMessage,
+		revokeMessage,
+		notPunishedMessage,
+		broadcastMessage;
 	
 	public static boolean canBeType(String type) {
 		try {
